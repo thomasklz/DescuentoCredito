cffe82c2acb4f8624f8382d0ea660347b4558e2d
Edson <edo_1199@yahoo.es>
2020-02-04 13:50:27 -0500

    Vista Proveedor

--- Creditos/Clases/clsProveedor.cs
+++ Creditos/Clases/clsProveedor.cs
@@ -8,8 +8,9 @@ using Creditos.Entity;
 namespace Creditos.Clases{
     public class clsProveedor{
         AdministracionAcademicaEntities db = new AdministracionAcademicaEntities();
+        clsProveedor clspro = new clsProveedor();
+        List<mProveedor> lista_proveedor = new List<mProveedor>();
         public List<mProveedor> mostrar(){
-            List<mProveedor> lista_proveedor = new List<mProveedor>();
             foreach (var item in db.spConsultarProveedor()){
                 lista_proveedor.Add(new mProveedor(item.id_proveedor, Convert.ToString(item.persona_juridica), Convert.ToString(item.RUC), Convert.ToString(item.nombre), Convert.ToString(item.descripcion), Convert.ToString(item.email), Convert.ToString(item.direccion), Convert.ToString(item.telefono)));
             }
@@ -18,11 +19,30 @@ namespace Creditos.Clases{
         public void ingresar(mProveedor datos){
             db.spInsertarProveedor(datos.persona_juridica, datos.RUC, datos.nombre, datos.descripcion, datos.email, datos.direccion, datos.telefono);
         }
-        public void eliminar(mProveedor datos){
-            db.spEliminarProveedor(datos.id_proveedor);
+        public void eliminar(int id){
+            db.spEliminarProveedor(id);
         }
-        public void modificar(mProveedor datos){
-            db.spModificarProveedor(datos.id_proveedor, datos.persona_juridica, datos.RUC, datos.nombre, datos.descripcion, datos.email, datos.direccion, datos.telefono);
+        public Boolean modificar(mProveedor datos)
+        {
+            Boolean result = false;
+            try
+            {
+                db.spModificarProveedor(datos.id_proveedor, datos.persona_juridica, datos.RUC, datos.nombre, datos.descripcion, datos.email, datos.direccion, datos.telefono);
+                result = true;
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            return result;
+        }
+        public List<mProveedor> mostrarById(int id)
+        {
+            foreach (var item in db.spConsultarProveedorById(id))
+            {
+                prov.Add(new mProveedor(item.id_proveedor, item.persona_juridica, item.RUC, item.nombre, item.descripcion, item.email, item.direccion, item.telefono));
+            }
+            return prov;
         }
     }
 }
